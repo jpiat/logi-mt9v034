865b2d6be026a38830e118bb614edb70c2c8f0ff
Jonathan Piat <piat.jonathan@gmail.com>
2015-07-06 16:18:36 +0200

    Added a Makefile for configuration exec

--- sw/mt9v034_grab/mt9v034_grab.c
+++ sw/mt9v034_grab/mt9v034_grab.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "logilib.h"
 
-#define BURST_SIZE 512
+#define BURST_SIZE 128
 
 #define FIFO_BASE_ADDR 0x200
 
@@ -43,10 +43,10 @@ void yuv_to_rgb(char * buffer, char * picture, int height, int width){
 
 
 int main(int argc, char ** argv){
-	unsigned int available, total ;
+	unsigned int available, total, avail_new, avail_old;
 	unsigned short cmd_word, sync_value ;
 	int count ;
-	unsigned int available_before ;
+	unsigned int available_before, cmd_long ;
 	FILE * pic_fd, * jpeg_fd ;
 	if(argc < 2){
 		printf("Specify output path \n");
@@ -59,8 +59,8 @@ int main(int argc, char ** argv){
 	jpeg_fd  = fopen(argv[1], "wb");
 	logi_open();
 	cmd_word = 0 ;
-	logi_read((unsigned char *) &cmd_word,sizeof(short),0x1000);
-	printf("sync errors : %u \n", cmd_word);
+	logi_read((unsigned char *) &cmd_long,sizeof(int),0x1000);
+	printf("sync errors : %x \n", cmd_long);
 	cmd_word = 1 ;
 	logi_write((unsigned char *) &cmd_word,sizeof(short),0x1000);//hold reset on Y2Fifo
 	cmd_word = 0x00 ;
@@ -87,20 +87,32 @@ int main(int argc, char ** argv){
 	count = 0 ;
 	while(count < (640*480*2)+4){
 		if(available > BURST_SIZE){
+			logi_read((unsigned char *) &avail_old, sizeof(int), FIFO_BASE_ADDR+1);
 			logi_read(&yuv_buffer[count], BURST_SIZE*2, 0x0000);
+			logi_read((unsigned char *) &avail_new, sizeof(int), FIFO_BASE_ADDR+1);
+			//printf("read : %u\n", (avail_old - avail_new));
+			if((avail_old - avail_new) != BURST_SIZE){
+				printf("fifo read problem : %u\n", (avail_old - avail_new));
+			}
 			available -= BURST_SIZE ;
 			count += BURST_SIZE*2 ;
 		}else{
+			logi_read((unsigned char *) &avail_old, sizeof(int), FIFO_BASE_ADDR+1);
 			logi_read(&yuv_buffer[count], available*2, 0x0000);
+			logi_read((unsigned char *) &avail_new, sizeof(int), FIFO_BASE_ADDR+1);
+                        //printf("read remaining: %u \n", (avail_old - avail_new));
+			if((avail_old - avail_new) != available){
+                                printf("fifo read problem : %u\n", (avail_old - avail_new));
+                        }
 			count += available*2;
 			available = 0 ;
 		}
 	}
 	logi_read((unsigned char *) &available, sizeof(int), FIFO_BASE_ADDR+1);
 	printf("read : %u, left : %u \n", (640*480)+2, available);
-	if((available_before - ((640*480)+2)) != available){
+	if((available_before - (count/2)) != available){
 		printf("Fifo access problem ! Data will be corrupted \n");
-		printf("There was %d missed accesses \n", (available_before - ((640*480)+2)-available));
+		printf("There was %d missed accesses \n", (available_before - (count/2)-available));
 	}
 	sync_value = 0x00 ;
 	sync_value = *((short *) &yuv_buffer[0]);
